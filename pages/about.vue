97fabd3caee2c296064434f79d80093219e0c1f3
Aleksey-gvr <68175964+Aleksey-gvr@users.noreply.github.com>
2021-02-05 00:14:47 +1000

    any commit

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse placeat hic
+      dolorum natus quae, cumque ut rerum nemo officia, quam porro. Saepe odit
+      totam placeat adipisci sunt assumenda ratione a!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse placeat hic
+      dolorum natus quae, cumque ut rerum nemo officia, quam porro. Saepe odit
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse placeat hic
+      dolorum natus quae, cumque ut rerum nemo officia, quam porro. Saepe odit
+      totam placeat adipisci sunt assumenda ratione a! totam placeat adipisci
+      sunt assumenda ratione a!
+    </p>
+  </section>
+</template>
